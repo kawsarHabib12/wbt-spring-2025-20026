4b7e62a9431c1bc54c72a68c4c3bd704f132f216
Sabbir-Hossain-03 <sabbirhossain01963@gmail.com>
2026-03-07 22:06:53 +0600

    first commit

---
+++ portfolio/js/script.js
@@ -0,0 +1,24 @@
+let a=3;
+let b=4;
+[a,b]=[b,a];
+
+console.log(a);
+console.log(b);
+
+function number(n)
+{
+   return n*n;
+}
+
+for(let i=0; i<=10;i++)
+{
+    console.log(number(i))
+}
+
+   
+
+const arr=[1,2,3,4,5];
+for(let i=0; i<arr.length;i++)
+{
+    console.log(arr[i]);
+}
